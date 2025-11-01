e54874a398fca8638f2cab94720b7c0ee0a59822
Pedro <pedrompro01@gmail.com>
2025-11-01 10:59:53 -0300

    Criação da classe MovimentoConta

---
+++ backend/controle_financeiro/src/main/java/com/projeto/domains/MovimentoConta.java
@@ -0,0 +1,123 @@
+package com.projeto.domains;
+
+import com.projeto.domains.enums.Status;
+import com.projeto.infra.StatusConverter;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Objects;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name="movimentoConta")
+@SequenceGenerator(
+        name = "seq_movimentoConta", // mesmo nome usado no @GeneratedValue
+        sequenceName = "seq_movimentoConta", // nome da sequência no banco
+        allocationSize = 1 // incrementa de 1 em 1 (evita “saltos”)
+)
+
+public class MovimentoConta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_movimentoConta")
+    private Long id;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    @Column(nullable = false)
+    private LocalDate dataMovimento;
+
+    @NotNull
+    @Digits(integer = 15, fraction = 3)
+    @Column(precision = 18, scale = 3, nullable = false)
+    private BigDecimal valor;
+
+    @NotBlank
+    @Column(nullable=false, length=100)
+    private String historico;
+
+    @Convert(converter = StatusConverter.class)
+    @Column(name = "status", nullable = false)
+    private Status status;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "idConta", nullable = false)
+    private ContaBancaria contaBancaria;
+
+    public MovimentoConta() {
+    }
+
+    public MovimentoConta(Long id, LocalDate dataMovimento, BigDecimal valor,
+                          String historico, Status status, ContaBancaria contaBancaria) {
+        this.id = id;
+        this.dataMovimento = dataMovimento;
+        this.valor = valor;
+        this.historico = historico;
+        this.status = status;
+        this.contaBancaria = contaBancaria;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDataMovimento() {
+        return dataMovimento;
+    }
+
+    public void setDataMovimento(LocalDate dataMovimento) {
+        this.dataMovimento = dataMovimento;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public String getHistorico() {
+        return historico;
+    }
+
+    public void setHistorico(String historico) {
+        this.historico = historico;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public ContaBancaria getContaBancaria() {
+        return contaBancaria;
+    }
+
+    public void setContaBancaria(ContaBancaria contaBancaria) {
+        this.contaBancaria = contaBancaria;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        MovimentoConta that = (MovimentoConta) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
