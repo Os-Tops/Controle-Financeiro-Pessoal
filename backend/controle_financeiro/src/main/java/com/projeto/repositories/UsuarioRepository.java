d125cea0bc888aa7a7470c9c642f9ed674fb41d9
Pedro Marques <pedrompro01@gmail.com>
2025-11-04 16:28:50 -0300

    Criação da Repository UsuarioRepository

---
+++ backend/controle_financeiro/src/main/java/com/projeto/repositories/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.projeto.repositories;
+
+import com.projeto.domains.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
